cc8724b2235297735a0af289efbe2e3fa58fb9fc
Manuel Goncalves <man281192@gmail.com>
2021-05-28 12:20:36 -0400

    feat: adding repository folder and files

---
+++ src/main/java/com/manuel28g/carsalesmobiletestdata/CovidDataApplication.kt
@@ -0,0 +1,18 @@
+//package com.manuel28g.carsalesmobiletestdata
+//
+//import android.content.Context
+//import au.com.carsales.basemodule.BaseModuleApplication
+//import au.com.carsales.basemodule.router.BaseModuleLifeCycleManager
+//import au.com.carsales.basemodule.router.Router
+//import com.manuel28g.carsalesmobiletestdata.service.CovidDataService
+//
+//open class CovidDataApplication:BaseModuleApplication() {
+//
+//    override fun initLifeCycleManager(): BaseModuleLifeCycleManager {
+//        return BaseModuleLifeCycleManager(this)
+//    }
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//    }
+//}
